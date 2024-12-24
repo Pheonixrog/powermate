b7f2eae37fb0dff8e1b28d248d6f9742ece0c6d7
Rishabh Katiyar <rishabhasus9@gmail.com>
2024-12-24 12:24:57 +0530

    updated

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 import { ScrollReveal } from './ScrollReveal'
 import { Button } from "@/components/ui/button"
 import { Card, CardHeader, CardContent, CardFooter } from "@/components/ui/card"
