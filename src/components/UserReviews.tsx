7ba02819eecde6bf3e4ee5cd7fa9f592dff439ec
Rishabh Katiyar <rishabhasus9@gmail.com>
2024-12-29 21:17:52 +0530

    u

--- src/components/UserReviews.tsx
+++ src/components/UserReviews.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { FullScreenSection } from './FullScreenSection'
+
 import { EnhancedTypography } from './EnhancedTypography'
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
