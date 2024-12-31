bc415dc04fb1777b93d32f7ddf31dd20ab62b991
Rishabh Katiyar <rishabhasus9@gmail.com>
2024-12-31 21:35:48 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,81 +28,46 @@ export default function Page() {
         <ScrollReveal>
           <section className="sm:pt-0 ">
             <div className="container mx-auto px-4">
-              <div className="flex flex-col items-center justify-center mt-10 ">
-                <h1 className=" md:mt-0 md:pt-0   text-[50px] md:text-[150px] lg:text-[180px] font-bold text-center overflow-hidden relative flex flex-wrap items-center justify-center gap-2 md:gap-4">
-                  <span className="relative z-10 leading-none">
-                    {"P".split("").map((char, index) => (
-                      <span
-                        key={index}
-                        className="inline-block transition-all bg-clip-text text-transparent bg-gradient-to-r from-emerald-700 to-emerald-200 duration-300 ease-in-out hover:scale-125 hover:text-emerald-200"
-                        style={{
-                          transitionDelay: `${index * 50}ms`,
-                          opacity: isVisible ? 1 : 0,
-                          transform: isVisible
-                            ? "translateY(0)"
-                            : "translateY(20px)",
-                        }}
-                      >
-                        {char}
-                      </span>
-                    ))}
-                  </span>
-                  <div className="flex items-center justify-center w-[50px] h-[50px] md:w-[280px] md:h-[280px] lg:w-[320px] lg:h-[320px]">
-                    <iframe
-                      className="w-full h-full"
-                      src="https://lottie.host/embed/86f8b081-70bc-4c9a-b73a-5ad19fa04101/58LqzG2pjg.lottie"
-                    ></iframe>
-                  </div>
-                  <span className="relative z-10 leading-none">
-                    {"wermate".split("").map((char, index) => (
-                      <span
-                        key={index}
-                        className="inline-block transition-all duration-300 bg-clip-text text-transparent bg-gradient-to-r from-emerald-700 to-emerald-200 ease-in-out hover:scale-125 hover:text-emerald-200"
-                        style={{
-                          transitionDelay: `${(index + 1) * 50}ms`,
-                          opacity: isVisible ? 1 : 0,
-                          transform: isVisible
-                            ? "translateY(0)"
-                            : "translateY(20px)",
-                        }}
-                      >
-                        {char}
-                      </span>
-                    ))}
-                  </span>
-                </h1>
-              </div>
-              <div className="sm:pt-0 sm:px-0 px-5 pt-10 grid md:mx-40  md:grid-rows-[2fr,1fr] lg:grid-cols-2 gap-5 lg:gap-16 items-center justify-center">
+              <div className="flex flex-col items-center justify-center mt-10 "></div>
+              <div className="sm:pt-0 sm:px-0 px-5 pt-10 grid md:mx-40   gap-5 lg:gap-16 items-center justify-center">
                 <div className=" text-center  lg:text-left ">
-                  <EnhancedTypography
-                    text="Control Your Screen Time With Friends"
-                    className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold mb-4 md:mb-6  "
-                    gradient={true}
-                  />
-                  <EnhancedTypography
-                    text="Stay focused and connected — because doom scrolling at 2 AM isn't the vibe."
-                    className="text-lg md:text-xl lg:text-2xl mb-6 md:mb-8 text-gray-300 "
-                  />
-                  <div className="sm:pt-0 pt-5 flex flex-col sm:flex-row gap-4 justify-center lg:justify-start">
-                    
-                    <button className="flex items-center justify-center gap-2 bg-black hover:bg-gray-800 text-white font-bold py-3 px-6 md:py-4  rounded-full text-base md:text-lg transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg">
-                      <Download size={24} />
-                      <a href="https://play.google.com/store/apps/details?id=com.appspot.powermateapp">
-                        Play Store
-                      </a>
-                    </button>
-                    <button className="flex items-center justify-center gap-2 bg-black hover:bg-gray-800 text-white font-bold py-3 px-6 md:py-4 md:px-8 rounded-full text-base md:text-lg transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg">
-                      <Apple size={24} />
-                      <a href="https://apps.apple.com/il/app/powermate-control-screen-time/id6535687314">
-                      App Store
-                      </a>
-    
-                     
-                    </button>
+                  <p className="text-5xl md:text-7xl lg:text-7xl xl:text-8xl font-bold mb-4 md:mb-6  text-[#CAFFE5]  ">
+                    Control Your Screen Time With Friends
+                  </p>
+                  <div className="flex flex-col md:flex-row lg:flex-row">
+                    <div className="flex flex-col  justify-center">
+                      <EnhancedTypography
+                        text="Stay focused and connected"
+                        className="text-xl md:text-3xl lg:text-4xl  md:mb-2 text-[#29A5D6] mx-3 md:mx-0"
+                      />
+                      <EnhancedTypography
+                        text="because doom scrolling at 2 AM isn't the vibe."
+                        className="text-lg md:text-2xl lg:text-3xl mb-6 md:mb-8 text-[#29A5D6] mx-3 md:mx-0"
+                      />
+                      <div className="sm:pt-0 pt-5 flex flex-col sm:flex-row gap-4 justify-center lg:justify-start">
+                        <button className="flex items-center justify-center gap-2 bg-black hover:bg-gray-800 text-white font-bold py-3 px-6 md:py-4  rounded-full text-base md:text-lg transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg">
+                          <Download size={24} />
+                          <a href="https://play.google.com/store/apps/details?id=com.appspot.powermateapp">
+                            Play Store
+                          </a>
+                        </button>
+                        <button className="flex items-center justify-center gap-2 bg-black hover:bg-gray-800 text-white font-bold py-3 px-6 md:py-4 md:px-8 rounded-full text-base md:text-lg transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg">
+                          <Apple size={24} />
+                          <a href="https://apps.apple.com/il/app/powermate-control-screen-time/id6535687314">
+                            App Store
+                          </a>
+                        </button>
+                      </div>
+                    </div>
+                    <div>
+                      <div className="sm:pt-0  sm:px-0  pt-20 mx-auto w-[300px] h-[300px] lg:w-[400px] lg:h-[400px] md:px-10">
+                        <img
+                          src="/Video mascots/Video mascots/GIF/7-download-animation.gif"
+                          alt=""
+                        />
+                      </div>
+                    </div>
                   </div>
-                </div>
-                <div className="sm:pt-0  sm:px-0  pt-20 mx-auto w-[300px] h-[300px] lg:w-[400px] lg:h-[400px]">
-                  <img src="/Video mascots/Video mascots/GIF/7-download-animation.gif" alt=""  />
                 </div>
               </div>
             </div>
