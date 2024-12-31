9be1632fabacc65d07d2649fae8989429a0034c9
Rishabh Katiyar <rishabhasus9@gmail.com>
2025-01-01 00:12:00 +0530

    please error fix ho ja

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { useState, useEffect } from "react";
+
 import { Navbar } from "@/components/Navbar";
 import { ScrollReveal } from "@/components/ScrollReveal";
 import { EnhancedTypography } from "@/components/EnhancedTypography";
